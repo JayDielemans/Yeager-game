b564785a67db1bff24d65bf3d5239fc1891387db
Jay Dielemans <Jay.dielemans@student.han.nl>
2022-05-31 21:37:22 +0200

    Initiële opzet Maven project
    Maven project met een dependency op de Yeager-engine

---
+++ The-Intelligent-Gamer/src/main/java/han/propedeuse/oopd/yeager/game/TheIntelligentGamer.java
@@ -0,0 +1,16 @@
+package han.propedeuse.oopd.yeager.game;
+
+import com.github.hanyaeger.api.YaegerGame;
+
+public class TheIntelligentGamer extends YaegerGame {
+
+    @Override
+    public void setupGame() {
+
+    }
+
+    @Override
+    public void setupScenes() {
+
+    }
+}
---
+++ The-Intelligent-Gamer/src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module waterworld {
+    requires hanyaeger;
+
+    exports han.propedeuse.oopd.yeager.game;
+
+    opens audio;
+    opens backgrounds;
+    opens sprites;
+}
