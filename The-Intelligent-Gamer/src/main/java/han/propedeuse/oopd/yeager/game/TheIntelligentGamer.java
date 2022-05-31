5125917a134e6bc75988af709f6cb59dbef18c8d
Jay Dielemans <Jay.dielemans@student.han.nl>
2022-05-31 21:49:45 +0200

    Main, grootte window en titel game.

--- The-Intelligent-Gamer/src/main/java/han/propedeuse/oopd/yeager/game/TheIntelligentGamer.java
+++ The-Intelligent-Gamer/src/main/java/han/propedeuse/oopd/yeager/game/TheIntelligentGamer.java
@@ -1,12 +1,18 @@
 package han.propedeuse.oopd.yeager.game;
 
+import com.github.hanyaeger.api.Size;
 import com.github.hanyaeger.api.YaegerGame;
 
 public class TheIntelligentGamer extends YaegerGame {
 
+    public static void main(String[] args) {
+       launch(args);
+    }
+
     @Override
     public void setupGame() {
-
+        setGameTitle("The Intelligent Gamer");
+        setSize(new Size(1500,750));
     }
 
     @Override
